913bb599ec1410f5e2b614585003021ba85d73a2
tanabe <tanabechankun@gmail.com>
2023-07-17 16:54:42 -0700

    more mobile optimization

--- script.js
+++ script.js
@@ -262,20 +262,14 @@ document.addEventListener("DOMContentLoaded", function() {
         menu.style.display = "grid";
     }
 
-
-    
-    startUp();
-    
-    $(document).ready(function() {
-        document.ontouchmove = function(e){
-             e.preventDefault();
-             }
-    });
-
     input.onfocus = function () {
         window.scrollTo(0, 0);
         document.body.scrollTop = 0;
     }
+
+    
+    startUp();
+
 
     /*
     function selectDeck() {
